34481f337638888af6f4f3899eec164db76a1dff
Dantts <gabrielteixeiradantas@hotmail.com>
2023-03-31 23:35:47 -0300

    feat: authentications route guard with public decorator and validations

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { TodoModule } from './todo/todo.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { TodoModule } from './modules/todo/todo.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './modules/auth/guards/auth.guard';
 
 @Module({
-  imports: [TodoModule],
+  imports: [TodoModule, PrismaModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
