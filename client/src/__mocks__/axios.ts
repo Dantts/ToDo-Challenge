1e75d16fdd16161f77fffa70297e2a6cbda88472
Dantts <gabrielteixeiradantas@hotmail.com>
2023-04-02 22:13:35 -0300

    config: jests config to try do tests running

---
+++ client/src/__mocks__/axios.ts
@@ -0,0 +1,17 @@
+export default {
+  get: jest.fn().mockResolvedValue({
+    data: {},
+  }),
+  patch: jest.fn().mockResolvedValue({
+    data: {},
+  }),
+  put: jest.fn().mockResolvedValue({
+    data: {},
+  }),
+  post: jest.fn().mockResolvedValue({
+    data: {},
+  }),
+  delete: jest.fn().mockResolvedValue({
+    data: {},
+  }),
+};
